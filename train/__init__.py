a43d2903a74b5b9c300c4a4bed3e4a9c0e323ba1
MannanB <mannanb728@gmail.com>
2026-01-17 21:06:08 -0500

    auto install deps

--- train/__init__.py
+++ train/__init__.py
@@ -1,9 +1,10 @@
 
 from .config import TinyLogicLMConfig
 from .train import train
+import os
 
 def main(run, config):
-
+    os.system("pip install zstandard")
     cfg = TinyLogicLMConfig(**config)
 
     train(run, cfg)
